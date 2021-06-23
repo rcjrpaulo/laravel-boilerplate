0540530ffbecffb145a5914f9e955a09f5801d8d
Paulo Cardoso <paulo@agily.com.br>
2021-06-23 13:28:30 -0300

    arquivos de traducao pra ptbr e trocando utc pra america sao paulo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->publishes(
+            [
+                __DIR__ . '/pt_BR.json' => resource_path('lang/pt_BR.json'),
+                __DIR__ . '/pt_BR' => resource_path('lang/pt_BR')
+            ],
+            'laravel-pt-br-localization'
+        );
     }
 
     /**
