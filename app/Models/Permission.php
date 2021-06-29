9becc946e708de7a6e68a7c9b892d9377e0c37d8
Paulo Cardoso <paulo@agily.com.br>
2021-06-29 15:45:46 -0300

    correcoes acl

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Permission extends Model
 {
+    use SoftDeletes;
+    use HasFactory;
+
 	protected $table = 'permissions';
 
 	protected $fillable = [
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-	use SoftDeletes;
+    use SoftDeletes;
+    use HasFactory;
 	protected $table = 'roles';
 
 	protected $fillable = [
