1bbf651714cdedb4806400d2e63ea2f92a4d510d
Paulo Cardoso <paulo@agily.com.br>
2021-06-30 11:41:49 -0300

    dashboard admin lte feito

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,21 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.adminlte')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+@section('main')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6"></div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active"><a href="javascript:void(0)">Home</a></li>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+    <div class="content">
+        <div class="container-fluid">
+
+            <h1>dashboard</h1>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -42,16 +42,71 @@
 </head>
 
 <body>
-<div id="app" class="bg-gray-100">
-    <header>
-        @include('layouts.header')
-    </header>
+<div id="app">
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a data-widget="pushmenu" href="javascript:void(0)" role="button" class="nav-link"><i class="fas fa-bars"></i></a></li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li>
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-white">Sair</button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
 
-    @include('layouts.alert')
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <a href="javascript:void(0)" class="brand-link">
+                <span class="brand-text font-weight-light">
+                    <img loading="lazy" src="dist/img/AdminLTELogo.png" width="50">
+                </span>
+            </a>
+            <div class="sidebar"><div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="dist/img/user2-160x160.jpg" alt="User Image" class="img-circle elevation-2">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">
+                            admin
+                        </a>
+                    </div>
+                </div>
 
-    <main class="container py-5">
-        @yield('main')
-    </main>
+                <nav class="mt-2">
+                    <ul data-widget="treeview" role="menu" data-accordion="false" class="nav nav-pills nav-sidebar flex-column pt-2">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link my-2 text-white">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+
+                        <li class="nav-header">PERMISSÕES</li>
+                        <li class="nav-item"><a href="/admin/roles" class="nav-link my-2 text-white"><i class="nav-icon fas fa-plus-square"></i> <p>Papéis</p></a></li>
+                        <li class="nav-item"><a href="/admin/users" class="nav-link my-2 text-white"><i class="nav-icon fas fa-user-alt"></i> <p>Usuários</p></a></li>
+                    </ul>
+
+                </nav>
+            </div>
+        </aside>
+
+        <div class="content-wrapper" style="min-height: 594.375px;">
+            <section class="content">
+                @include('layouts.alert')
+                @yield('main')
+            </section>
+        </div>
+
+        <footer class="main-footer text-center">
+            <span class="text-muted"><span>Feito com muito</span> <span><i class="fa fa-heart text-danger fa-pulse"></i></span> <span>pela <a href="http://www.agily.com.br" target="_blank">Agily Tecnologia</a></span></span>
+        </footer>
+
+        <div id="sidebar-overlay"></div>
+    </div>
+
 
     <footer>
         @include('layouts.footer')
