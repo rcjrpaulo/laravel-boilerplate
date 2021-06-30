03403d3c5278120b811bc163a8023bd6c11d0213
Paulo Cardoso <paulo@agily.com.br>
2021-06-30 16:43:23 -0300

    user-profile-photo

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -66,7 +66,7 @@
             </a>
             <div class="sidebar"><div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" alt="User Image" class="img-circle elevation-2">
+                        <img src="{{ auth()->user()->photoUrl ?? 'images/profile.png' }}" alt="User Image" class="img-circle elevation-2">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">
