9d72a0edfa6344108dabf6444e60d4f875b3c027
Paulo Cardoso <paulo@agily.com.br>
2021-07-01 19:33:07 -0300

    input type email

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -28,7 +28,7 @@
 
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="text" id="email" name="email" class="form-control" required>
+                    <input type="email" id="email" name="email" class="form-control" required>
                 </div>
 
                 <div class="form-group">
