4eadb537f79b698a6d14d584076ce6d985add404
Paulo Cardoso <paulo@agily.com.br>
2021-07-01 19:39:54 -0300

    removido form de show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -18,47 +18,44 @@
         <div class="card-header">
             <h3 class="card-title">Ver usuário</h3>
         </div>
-        <form action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data" role="form">
-            @csrf
-            <div class="card-body">
-                <div class="form-group">
-                    <label for="name">Nome</label>
-                    <input value="{{ $user->name }}" type="text" id="name" name="name" class="form-control" required disabled>
-                </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="name">Nome</label>
+                <input value="{{ $user->name }}" type="text" id="name" name="name" class="form-control" required disabled>
+            </div>
 
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input value="{{ $user->email }}" type="email" id="email" name="email" class="form-control" required disabled>
-                </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input value="{{ $user->email }}" type="email" id="email" name="email" class="form-control" required disabled>
+            </div>
 
-                <div class="form-group">
-                    <label for="password">Senha</label>
-                    <input value="" type="password" id="password" name="password" class="form-control" required disabled>
-                </div>
+            <div class="form-group">
+                <label for="password">Senha</label>
+                <input value="" type="password" id="password" name="password" class="form-control" required disabled>
+            </div>
 
-                <div class="form-group">
-                    <label for="role_id">Papel</label>
-                    <input value="{{ $user->role->label ?? 'Sem Papel' }}" type="text" id="role_id" name="role_id" class="form-control" required disabled>
-                </div>
+            <div class="form-group">
+                <label for="role_id">Papel</label>
+                <input value="{{ $user->role->label ?? 'Sem Papel' }}" type="text" id="role_id" name="role_id" class="form-control" required disabled>
+            </div>
 
-                <div class="form-group">
-                    <label for="photo">Foto</label>
-                    <div class="input-group">
-                        <div class="custom-file">
-                            <input value="{{ $user->id }}" type="file" id="photo" name="photo" class="custom-file-input" disabled>
-                            <label for="photo" class="custom-file-label">Escolha uma foto</label>
-                        </div>
-                    </div>
-                    <div class="preview mt-3">
-
+            <div class="form-group">
+                <label for="photo">Foto</label>
+                <div class="input-group">
+                    <div class="custom-file">
+                        <input value="{{ $user->id }}" type="file" id="photo" name="photo" class="custom-file-input" disabled>
+                        <label for="photo" class="custom-file-label">Escolha uma foto</label>
                     </div>
                 </div>
+                <div class="preview mt-3">
+
+                </div>
             </div>
-            <div class="card-footer">
-                <a href="{{ route('users.index') }}" class="btn btn-primary">
-                    Voltar
-                </a>
-            </div>
-        </form>
+        </div>
+        <div class="card-footer">
+            <a href="{{ route('users.index') }}" class="btn btn-primary">
+                Voltar
+            </a>
+        </div>
     </div>
 @endsection
