4c958c3fcd9c4263cd4a2a88a784c6bbd7dc7cf4
Paulo Cardoso <paulo@agily.com.br>
2021-07-01 19:36:02 -0300

    id corrigido

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -46,7 +46,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="profile_photo">Foto</label>
+                    <label for="photo">Foto</label>
                     <div class="input-group">
                         <div class="custom-file">
                             <input type="file" id="photo" name="photo" class="custom-file-input">
